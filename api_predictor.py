6668a70af427ff487d18c588c31fdfd4655468ec
Yang82ying <yang82ying@gmail.com>
2025-07-08 17:23:07 -0600

    Update api_predictor.py

--- api_predictor.py
+++ api_predictor.py
@@ -37,8 +37,8 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-if __name__ == "__main__":
-    import os
+import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render usa PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
